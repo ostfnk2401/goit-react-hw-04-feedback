36f064f166f7a0b99268f5e772c230becb9a9779
ostfnk2401 <o.stfnk2401@gmail.com>
2023-04-27 00:41:41 +0300

    add Section import to feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FeedbackForm, FeedbackWrapper } from './Feedback.styled.jsx';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
 import { Statistics } from "./Statistics/Statistics.jsx";
+import { Section } from "./Section/Section.jsx";
 
 
 export class Feedback extends React.Component {
