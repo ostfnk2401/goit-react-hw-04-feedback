3530f8e51619f165a502fee96642f7dedcf7f7d0
ostfnk2401 <o.stfnk2401@gmail.com>
2023-04-26 23:28:24 +0300

    fix onLeaveFeedback

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FeedbackList, FeedbackItem, FeedbackButton } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({ options, leaveFeedback }) => {
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
         <FeedbackList>
             {options.map((option) =>{
                 const buttonOption = option.charAt(0).toUpperCase() + option.slice(1);
                 return(
                     <FeedbackItem key={options}>
-                        <FeedbackButton type='button' prop={option} onClick={() => leaveFeedback(option)}>
+                        <FeedbackButton type='button' prop={option} onClick={() => onLeaveFeedback(option)}>
                             {buttonOption}
                         </FeedbackButton>
                     </FeedbackItem>
