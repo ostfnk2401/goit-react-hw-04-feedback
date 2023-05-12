7e31311f929fbe9ff76c7a1cbb2dff6169e79eb3
ostfnk2401 <o.stfnk2401@gmail.com>
2023-05-12 12:04:24 +0300

    using hooks in FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FeedbackList, FeedbackItem, FeedbackButton } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (
-        <FeedbackList>
-            {options.map((option) =>{
-                const buttonOption = option.charAt(0).toUpperCase() + option.slice(1);
-                return(
-                    <FeedbackItem key={options}>
-                        <FeedbackButton type='button' prop={option} onClick={() => onLeaveFeedback(option)}>
-                            {buttonOption}
-                        </FeedbackButton>
-                    </FeedbackItem>
-                );
-            })}
-        </FeedbackList>
-    );
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    onLeaveFeedback(option);
+  };
+
+  return (
+    <FeedbackList>
+      {options.map((option) => {
+        const buttonOption = option.charAt(0).toUpperCase() + option.slice(1);
+        return (
+          <FeedbackItem key={option}>
+            <FeedbackButton
+              type='button'
+              prop={option}
+              onClick={() => handleOptionClick(option)}
+              selected={option === selectedOption}
+            >
+              {buttonOption}
+            </FeedbackButton>
+          </FeedbackItem>
+        );
+      })}
+    </FeedbackList>
+  );
 };
 
 FeedbackOptions.propTypes = {
-    onLeaveFeedback: PropTypes.func.isRequired,
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  }
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default FeedbackOptions;
